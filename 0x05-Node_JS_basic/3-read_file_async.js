91810a916b84f1ad39744ff960c0d00b8a3f1b30
Swittee Franca <190401022@live.unilag.edu.ng>
2023-08-23 04:41:41 +0000

    Node

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -1,35 +1,37 @@
 const fs = require('fs');
 
-const { parse } = require('csv-parse');
-
-const countStudents = (file) => {
+const countStudents = (file) => new Promise((res, rej) => {
+  const names = [];
   let num = 0;
   let numCs = 0;
   let numSwe = 0;
-  const names = [];
-  const cs = [];
-  const swe = [];
-  fs.createReadStream(file)
-    .pipe(parse({ delimeter: ',', from_line: 2 }))
-    .on('data', (row) => {
-      num += 1;
-      names.push(row[0]);
-      if (row[3] === 'CS') {
-        cs.push(row[0]);
-        numCs += 1;
-      }
-      if (row[3] === 'SWE') {
-        swe.push(row[0]);
-        numSwe += 1;
-      }
-    })
-    .on('end', () => {
+  const Cs = [];
+  const Swe = [];
+  fs.readFile(file, 'utf8', (err, data) => {
+    if (err) {
+      rej(new Error('Cannot load the database'));
+    } else {
+      const lines = data.split('\n');
+      lines.slice(1).forEach((line) => {
+        const fields = line.split(',');
+        if (line !== '') {
+          num += 1;
+          names.push(fields[0]);
+        }
+        if (fields[3] === 'CS') {
+          numCs += 1;
+          Cs.push(fields[0]);
+        }
+        if (fields[3] === 'SWE') {
+          numSwe += 1;
+          Swe.push(fields[0]);
+        }
+      });
       console.log(`Number of students: ${num}. List: ${names}`);
-      console.log(`Number of students in CS: ${numCs}. List: ${cs}`);
-      console.log(`Number of students in SWE: ${numSwe}. List: ${swe}`);
-    })
-    .on('error', () => {
-      throw new Error('Cannot load the database');
-    });
-};
+      console.log(`Number of students in CS: ${numCs}. List: ${Cs}`);
+      console.log(`Number of students in SWE: ${numSwe}. List: ${Swe}`);
+      res();
+    }
+  });
+});
 module.exports = countStudents;
