e0bbb00c7bad9d9983aa7bc04bd588a8560a6118
Swittee Franca <190401022@live.unilag.edu.ng>
2023-08-23 03:56:15 +0000

    updating 2

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,7 +1,5 @@
 const fs = require('fs');
 
-const { parse } = require('csv-parse');
-
 const countStudents = (file) => {
   let num = 0;
   let numCs = 0;
@@ -9,27 +7,29 @@ const countStudents = (file) => {
   const names = [];
   const cs = [];
   const swe = [];
-  fs.createReadStream(file)
-    .pipe(parse({ delimeter: ',', from_line: 2 }))
-    .on('data', (row) => {
-      num += 1;
-      names.push(row[0]);
-      if (row[3] === 'CS') {
-        cs.push(row[0]);
+  try {
+    const data = fs.readFileSync(file, 'utf8');
+    const lines = data.split('\n');
+    lines.slice(1).forEach((line) => {
+      const fields = line.split(',');
+      if (line !== '') {
+        num += 1;
+        names.push(fields[0]);
+      }
+      if (fields[3] === 'CS') {
+        cs.push(fields[0]);
         numCs += 1;
       }
-      if (row[3] === 'SWE') {
-        swe.push(row[0]);
+      if (fields[3] === 'SWE') {
+        swe.push(fields[0]);
         numSwe += 1;
       }
-    })
-    .on('end', () => {
-      console.log(`Number of students: ${num}. List: ${names}`);
-      console.log(`Number of students in CS: ${numCs}. List: ${cs}`);
-      console.log(`Number of students in SWE: ${numSwe}. List: ${swe}`);
-    })
-    .on('error', () => {
-      throw new Error('Cannot load the database');
     });
+    console.log(`Number of students: ${num}. List: ${names}`);
+    console.log(`Number of students in CS: ${numCs}. List: ${cs}`);
+    console.log(`Number of students in SWE: ${numSwe}. List: ${swe}`);
+  } catch (e) {
+    throw new Error('Cannot load the database');
+  }
 };
 module.exports = countStudents;
