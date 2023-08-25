a70463b890a15a4634d3cb49f9098713646b9b69
Swittee Franca <190401022@live.unilag.edu.ng>
2023-08-25 17:02:32 +0000

    unittesting

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -1,18 +1,20 @@
-onst request = require('supertest');
-const app = require('./api'); // Assuming api.js is in the same directory
-
+const request = require('request');
+const app = require('./api'); 
+const {expect}= require('chai')
 describe('API', () => {
     describe('GET /', () => {
         it('should have the correct status code', (done) => {
-            request(app)
-                .get('/')
-                .expect(200, done);
+            request.get('http://localhost:7865', (error, response) => {
+                    expect(response.statusCode).to.equal(200);
+                    done();
+                });
         });
 
         it('should return the correct message', (done) => {
-            request(app)
-                .get('/')
-                .expect('Welcome to the payment system', done);
+            request.get('http://localhost:7865', (error, response, body) => {
+                    expect(body).to.equal('Welcome to the payment system');
+                    done();
+                });
         });
     });
 });
---
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -0,0 +1,48 @@
+const request = require('request');
+const app = require('./api'); 
+const {expect}= require('chai')
+describe('API', () => {
+    describe('GET /', () => {
+        it('should have the correct status code', (done) => {
+            request.get('http://localhost:7865', (error, response) => {
+                    expect(response.statusCode).to.equal(200);
+                    done();
+                });
+        });
+
+        it('should return the correct message', (done) => {
+            request.get('http://localhost:7865', (error, response, body) => {
+                    expect(body).to.equal('Welcome to the payment system');
+                    done();
+                });
+        });
+    });
+    describe('GET /cart/:id', () => {
+        it('should have the correct status code', (done) => {
+            request.get('http://localhost:7865/cart/12', (error, response) => {
+                    expect(response.statusCode).to.equal(200);
+                    done();
+                });
+        });
+
+        it('should return the correct status code', (done) => {
+            request.get('http://localhost:7865/cart/hello', (error, response) => {
+                    expect(response.statusCode).to.equal(404);
+                    done();
+                });
+        });
+        it('should have the correct body', (done) => {
+            request.get('http://localhost:7865/cart/12', (error, response, body) => {
+                    expect(body).to.equal('Payment methods for cart 12');
+                    done();
+                });
+        });
+
+        it('should return the correct body', (done) => {
+            request.get('http://localhost:7865/cart/hello', (error, response, body) => {
+                    expect(body).to.equal('');
+                    done();
+                });
+        });
+    });
+});
